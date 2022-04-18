13fa492e796ad5ac8880220a650a5a4f4dedaec1
riicky-hub <richardjosephace1960@gmail.com>
2022-04-18 20:44:13 -0300

    finalizada base

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+function criaCalculadora() {
+    return {
+        display: document.querySelector('.display'),
+        btnClear: document.querySelector('.btn-clear'),
+        inicia() {
+            this.cliqueBotoes();
+        },
+        cliqueBotoes() {
+            document.addEventListener('click', (e) => {
+                const el = e.target;
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+                if(el.classList.contains('btn-clear')) {
+                    this.display.value = '';
+                }
+                if(el.classList.contains('btn-del')) {
+                    this.deleteOne();
+                }
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+            });
+        },
+        realizaConta() {
+            let conta = this.display.value;
+            try {
+                conta = eval(conta);
+                if(!conta) {
+                    alert('Conta inválida');
+                    return;
+                }
+                this.display.value = String(conta);
+            } catch(e) {
+                alert('Conta inválida')
+                return;
+            }
+        },
+        deleteOne() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        }
+    };
+}
+const calculadora = criaCalculadora();
+calculadora.inicia();
