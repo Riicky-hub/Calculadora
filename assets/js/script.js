d4d20e4eb0009f55c4a8d6d93a7ed35229d86282
riicky-hub <richardjosephace1960@gmail.com>
2022-07-06 20:48:44 -0300

    recriando calculadora com jQuery e animacoes

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// AOS
+AOS.init();
+// calculadora
+jQuery(($) => {
+    let input = $('.display');
+    function botoes(){
+        $(document).on('click', (e) => {
+            const el = e.target;
+            if(el.classList.contains('btn-num')) {
+                input.val(input.val() + el.textContent);
+            }
+            if(el.classList.contains('btn-clear')) {
+                input.val(''); 
+            }
+            if(el.classList.contains('btn-del')) {
+                input.val(input.val().slice(0, -1));
+            }
+            if(el.classList.contains('btn-eq')) {
+                equal(input);
+            }
+        });
+    }
+    function equal(i) {
+        let conta = i.val();
+        conta = eval(conta);
+        if(!conta) alert('Conta inválida!');
+        i.val(String(conta));
+    }
+    botoes();
+})
